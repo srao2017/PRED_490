f6a09285dc6b53097109ed9879fd7929347dfedf
srao2017 <sanjayrao2017@u.northwestern.edu>
2017-04-19 19:12:37 -0400

    Update LetterPatterns.py

--- LetterPatterns.py
+++ LetterPatterns.py
@@ -13,7 +13,7 @@ def get_letters (filename, letter):
         for row in reader:
             #find the letter
             if row[0] in letter:
-                print(row)
+                print(row[0])
                 i = 0;
                 continue
             #retrieve the pattern and collapse into one row
@@ -22,22 +22,22 @@ def get_letters (filename, letter):
                 i+=1
     return pattern
 
-def put_letters (filename, pattern, letter_id, letter):
-       
-    f = open(filename+str("_out.txt"),"ab")
+def put_letters (f, pattern, letter_id, letter):
+
     writer = csv.writer(f)
     writer.writerow(pattern+list([letter_id,str("'")+letter+str("'")]))
-    f.close()
-    
+
 def main():
-    out_dir = "C:/EDUCATION/NWU/490 - Deep Learning/Week 4"
+    out_dir = "C:/EDUCATION/NWU/490 - Deep Learning/Week 4/Create Letter Patterns"
     filename = "ABCD"
-    letters = list(['A','B','X'])
+    letters = list(['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'])
     os.chdir(out_dir)
+    f = open(filename+str("_out.txt"),"wb")
     
     for letter in letters:
         letter_id = ord(letter) - ord('A') 
         pattern = get_letters(str(filename)+str(".csv"),[letter])
-        put_letters(filename,pattern, letter_id, letter)
-    
+        put_letters(f,pattern, letter_id, letter)
+        
+    f.close()
 if __name__ == "__main__": main()
